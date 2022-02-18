053496044c8814a6fbe0554fd81ce66ec126d51e
Diego Schmitt <diego _asms@hotmail.com>
2022-02-18 12:52:08 -0300

    Projeto desenvolvido durante treinamento para consumo de uma API

---
+++ script.js
@@ -0,0 +1,50 @@
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
+    event.preventDefault(); //previne que o formulário seja enviado
+
+    let input = document.querySelector('#searchInput').value
+    if(input != ''){ 
+        clearInfo();
+        showWarning('Carregando...');
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=93dcf9f5e5b72ba7d7fa85934f9bfbf1&units=metric&lang=pt_br`
+        let results = await fetch(url)
+        let json = await results.json()
+
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })
+        }else{
+            clearInfo();
+            showWarning('Não encontramos essa localização.');
+        }
+    } else {
+        clearInfo();
+    }
+
+})
+
+function showInfo(json){
+    showWarning('')
+   
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed}<span> Km/h</span>`
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`
+
+    document.querySelector('.resultado').style.display = 'block'
+}
+
+function clearInfo(){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+}
+
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg;
+}
